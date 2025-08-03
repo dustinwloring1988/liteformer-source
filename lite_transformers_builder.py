f14ac3377c34967320e73d63c15a548e27136260
Dustin Loring <dustinwloring1988@gmail.com>
2025-08-02 20:19:56 -0400

    Update lite_transformers_builder.py

--- lite_transformers_builder.py
+++ lite_transformers_builder.py
@@ -10,8 +10,8 @@ REPO_URL = "https://github.com/huggingface/transformers.git"
 TARGET_DIR = "lite_transformers"
 
 NEW_MODELS = [
-    "aformer", "bformer", "cformer", "dformer", "eformer", "mformer",
-    "nformer", "oformer", "sformer", "tformer", "vformer"
+    "aformer", "bformer", "cformer", "dformer", "eformer", "fformer",
+    "mformer", "nformer", "oformer", "sformer", "tformer", "vformer"
 ]
 
 PRESERVE_MODELS = [
